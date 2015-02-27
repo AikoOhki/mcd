12b38c89e0b782631e527bb5c1e878f429628cd4
Aiko <aiko.alpacaxcute@gmail.com>
2015-02-27 23:28:51 +0900

    クローゼットからギャラリーに

--- app/src/main/java/com/example/aiko/mcd/Closeet.java
+++ app/src/main/java/com/example/aiko/mcd/Closeet.java
@@ -1,17 +1,85 @@
 package com.example.aiko.mcd;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
 
 
 public class Closeet extends ActionBarActivity {
+
+    private static final int REQUEST_GALLERY = 1;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_closeet);
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void outer (View v){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, REQUEST_GALLERY);
+
+    }
+
+    public void tops (View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, REQUEST_GALLERY);
+    }
+
+    public void bottoms (View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, REQUEST_GALLERY);
+    }
+
+    public void dress (View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, REQUEST_GALLERY);
+    }
+
+    public void shoes (View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, REQUEST_GALLERY);
+    }
+
+    public void accessories (View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, REQUEST_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_GALLERY && resultCode == RESULT_OK) {
+            try {
+                InputStream input = getContentResolver().openInputStream(data.getData());
+                Bitmap image = BitmapFactory.decodeStream(input);
+                input.close();
+
+                imageView.setImageBitmap(image);
+            } catch (Exception e) {
+            }
+        }
     }
 
 
